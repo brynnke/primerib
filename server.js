28219104486ce09f853a82d44a0998f730fbc21a
brynnke <100329799+brynnke@users.noreply.github.com>
2022-06-01 20:53:23 -0500

    section query working on function and viewing function/query

--- server.js
+++ server.js
@@ -85,6 +85,28 @@ function viewEmployee () {
 
         console.table(res);
         console.log("employees viewed \n");
-    })
 
+        firstPrompt ();
+    });
+
+}
+
+// section array
+
+function viewEmployeeBySection () {
+    console.log("Viewing employees by department\n");
+
+    var query =
+    `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`
+
+    connection.query(query, function (err, res){
+        
+    })
+  
 }
